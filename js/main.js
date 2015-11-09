58e5828caadd41e930df59b886c332f70946e03b
antonija_simic <antonija1023@gmail.com>
2015-11-09 13:37:08 +0100

    add more smoothness to sidebar

--- js/main.js
+++ js/main.js
@@ -2,27 +2,31 @@ $(document).ready(function(){
   runPreloader();
   $(".sidebar-open").click(function(){
    openNav();
-  });
+ });
 
   $(".close, .sidebar-item").click(function(){
    closeNav();
-  })
+ })
 
   function openNav() {
-     $(".sidebar-con").animate({
-      right: 0
-    }, 500, function(){
-    });
-  }
+   $(".sidebar-con").css("display", "block")
+   $(".sidebar-con").animate({
+    right: 0
+  },1000, function(){
+      
+  });
+ }
 
-  function closeNav() {
-     $(".sidebar-con").animate({
-      right: "-999px"
-    }, 500, function(){
-    });
-  }
+ function closeNav() {
+   $(".sidebar-con").animate({
+    right: "-999px"
+  }, 1000, function(){
+    $(".sidebar-con").css("display", "none")
 
-  $(function() {
+  });
+ }
+
+ $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -37,10 +41,11 @@ $(document).ready(function(){
   });
 });
 
-  function runPreloader () {
+
+ function runPreloader () {
   $('.preloader-con').delay(3000).fadeOut('slow'); 
   setTimeout(function(){
     $("body, html").css("overflow", "visible");
   }, 3000)
-}
+  }
 });
